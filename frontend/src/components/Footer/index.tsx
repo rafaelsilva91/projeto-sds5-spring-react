7537aa209769c1d90ddeac8235669980f523ed44
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2021-11-06 11:56:26 -0400

    LinkedIn

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/rafaelsilva91" target="_blank" rel="noreferrer">Rafael S. Rodrigues</a></p>
-                <p className="text-light">LinkedIn <a href="linkedin.com/in/rafael-s-rodrigues-616b88167" target="_blank" rel="noreferrer">Rafael S. Rodrigues</a></p>
+                <p className="text-light">LinkedIn <a href="https://www.linkedin.com/in/rafael-s-rodrigues-616b88167/" target="_blank" rel="noreferrer">Rafael S. Rodrigues</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
