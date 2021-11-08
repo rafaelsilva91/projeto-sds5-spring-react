76a2b10b74b144b2f2ac1feecd9b41afcdc848a8
rafaelsilva91 <rafaelsilvarodrigues_@hotmail.com>
2021-11-07 22:40:18 -0400

    DonutChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,26 +11,29 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //Forma errada
-    let chartData : ChartData ={labels: [], series: []}
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //Forma errada
-    axios.get(`${BASE_URL}/api/sales/amount-by-seller`)
-        .then( response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            
-            chartData = {labels: myLabels, series: mySeries}
+    useEffect(() => {
+        axios.get(`${BASE_URL}/api/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            console.log(chartData)
-        } )
+                setChartData({ labels: myLabels, series: mySeries })
+
+                console.log(chartData)
+        })
+    },[])
+
+ 
+
 
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-    
+
     const options = {
         legend: {
             show: true
@@ -37,7 +41,7 @@ const DonutChart = () => {
     }
     return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
